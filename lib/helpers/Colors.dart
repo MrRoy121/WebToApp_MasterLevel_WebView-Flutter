a7b66f650e05beaac3394b81438b9b13de5904e1
Nilashish Roy <nilashishroyjoy@gmail.com>
2023-02-10 05:09:00 +0600

    Added Colors

---
+++ lib/helpers/Colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryColors = MaterialColor(
+  0xFF30475E,
+  <int, Color>{
+    50: primaryColor,
+    100: primaryColor,
+    200: primaryColor,
+    300: primaryColor,
+    400: primaryColor,
+    500: primaryColor,
+    600: primaryColor,
+    700: primaryColor,
+    800: primaryColor,
+    900: primaryColor,
+  },
+);
+
+const Color primaryColor = Color(0xFF000000);
+const Color backgroundColorDarkTheme = Color(0xFF000000);
+const Color accentColor = Color(0xFFC92BCE);
+const Color splashBackColor = Color(0xFF13123F);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Color(0xFF000000);
+const Color onboardingButtonColor1 = Color(0xFF21005D);
+const Color onboardingButtonColor2 = Color(0xFFD932C7);
+const Color onboardingBGColor = Color(0xFFFFFFFF);
