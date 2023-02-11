3a1b9e2636e62a1eb3a17d21391c473ce2df9d5f
Nilashish Roy <nilashishroyjoy@gmail.com>
2023-02-12 05:09:00 +0600

    Added Const

---
+++ lib/helpers/Constant.dart
@@ -0,0 +1,64 @@
+const appName = 'Web to App';
+
+const String appbartitle = 'Web to App';
+const String andoidPackageName = 'com.example.webtoapp_flutter';
+
+//change this url to set your URL in app
+String webinitialUrl = 'https://website.com/';
+const String firstTabUrl = 'https://website.com/';
+
+//keep local content of pages of setting screen
+const String aboutPageURL = '';
+const String privacyPageURL = '';
+const String termsPageURL = '';
+
+//Change App id of android and IOS app
+const String androidAppId = andoidPackageName;
+
+const String iOSAppId = '';
+
+const String shareAppTitle = appName;
+const String shareiOSAppMessage = '';
+const String shareAndroidAppMessge =
+    'Download $appName App from this link : http://play.google.com/store/apps/details?id=$androidAppId';
+
+//To turn on/off ads
+const bool showInterstitialAds = false;
+const bool showBannerAds = false;
+const bool showOpenAds = false;
+
+//To turn on/off display of bottom navigation bar
+const bool showBottomNavigationBar = false;
+
+//To show/remove splash screen
+const bool showSplashScreen = false;
+
+//To show/remove splash screen
+const bool showOnboardingScreen = false;
+
+//To remove/display header/footer of website
+const bool hideHeader = false;
+const bool hideFooter = false;
+
+//Ids for interstitial Ad
+const androidInterstitialAdId = '';
+const iosInterstitialAdId = '';
+
+//Ids for banner Ad
+const androidBannerAdId = '';
+const iosBannerAdId = '';
+
+//Ids for app open Ad
+const androidOpenAdId = '';
+const iosOpenAdId = '';
+
+//icon to set when get firebase messages
+const String messageIcon = '@mipmap/ic_launcher_squircle';
+
+//copy your  one-signal android app id
+const oneSignalAndroidAppIds = '';
+
+//copy your  one-signal ios app id
+const oneSignalIOSAppId = '';
+
+const String iconPath = 'assets/icons/';
