0db25e4fed53d30d21b15485f86d8d6df2d7422b
Nilashish Roy <nilashishroyjoy@gmail.com>
2023-02-16 02:09:00 +0600

    Added String

---
+++ lib/helpers/Strings.dart
@@ -0,0 +1,51 @@
+class CustomStrings {
+  static const String home = 'Home';
+
+  static const String demo = 'Try Demo';
+
+  static const String settings = 'Settings';
+
+  static const String darkMode = 'Dark Mode';
+
+  static const String aboutUs = 'About Us';
+
+  static const String privacyPolicy = 'Privacy Policy';
+
+  static const String terms = 'Terms & Conditions';
+
+  static const String share = 'Share';
+
+  static const String rateUs = 'Rate Us';
+
+  //edit these 2 string for No Internet connectivity page
+  static const String noInternet1 = 'No Internet Connection!';
+  static const String noInternet2 = 'Please try again';
+
+  //edit these 2 strings for Page not found error page
+  static const String pageNotFound1 = 'Something went wrong!';
+  static const String pageNotFound2 = 'Page Not Found';
+
+  //edit these 2 strings for Incorrect URL error page
+  static const String incorrectURL1 = 'Sory, We couldn\'t find this page.';
+  static const String incorrectURL2 = 'Please chaeck your URL';
+
+// below 3 string are for Abouts Us, Privacy Policy and Terms & Conditions page pf Setting screen
+  static const String aboutPageContent =
+      '<html><body><h3>About Us</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet. Nec dui nunc mattis enim ut tellus. Lacus suspendisse faucibus interdum posuere lorem ipsum. Augue neque gravida in fermentum et sollicitudin ac. Est ante in nibh mauris cursus mattis molestie a. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Semper eget duis at tellus at urna condimentum mattis pellentesque. Enim sed faucibus turpis in eu. At auctor urna nunc id cursus metus aliquam. <br><br>Sed pulvinar proin gravida hendrerit lectus. Varius quam quisque id diam vel quam elementum pulvinar etiam. Sed risus ultricies tristique nulla. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Cursus eget nunc scelerisque viverra mauris in aliquam. Purus sit amet volutpat consequat mauris nunc. Faucibus purus in massa tempor nec feugiat nisl pretium. Id interdum velit laoreet id. Auctor eu augue ut lectus. \n\n Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Eget lorem dolor sed viverra ipsum nunc aliquet. Leo duis ut diam quam. Scelerisque in dictum non consectetur a erat nam at lectus. Augue ut lectus arcu bibendum at varius vel. Arcu risus quis varius quam quisque. Enim ut sem viverra aliquet eget sit amet tellus. Ut diam quam nulla porttitor. Pharetra vel turpis nunc eget lorem dolor sed. Turpis massa sed elementum tempus egestas sed. Natoque penatibus et magnis dis parturient montes. Elit at imperdiet dui accumsan sit amet nulla facilisi. Vulputate dignissim suspendisse in est ante in nibh mauris. Eu facilisis sed odio morbi. Cum sociis natoque penatibus et magnis dis parturient. Sed vulputate odio ut enim blandit. Etiam dignissim diam quis enim lobortis scelerisque. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Id donec ultrices tincidunt arcu non sodales.</p></body></html>';
+  static const String privacyPageContent =
+      '<html><body><h3>Privacy Policy</h3><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet. Nec dui nunc mattis enim ut tellus. Lacus suspendisse faucibus interdum posuere lorem ipsum. Augue neque gravida in fermentum et sollicitudin ac. Est ante in nibh mauris cursus mattis molestie a. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Semper eget duis at tellus at urna condimentum mattis pellentesque. Enim sed faucibus turpis in eu. At auctor urna nunc id cursus metus aliquam. <br><br>Sed pulvinar proin gravida hendrerit lectus. Varius quam quisque id diam vel quam elementum pulvinar etiam. Sed risus ultricies tristique nulla. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Cursus eget nunc scelerisque viverra mauris in aliquam. Purus sit amet volutpat consequat mauris nunc. Faucibus purus in massa tempor nec feugiat nisl pretium. Id interdum velit laoreet id. Auctor eu augue ut lectus. \n\n Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Eget lorem dolor sed viverra ipsum nunc aliquet. Leo duis ut diam quam. Scelerisque in dictum non consectetur a erat nam at lectus. Augue ut lectus arcu bibendum at varius vel. Arcu risus quis varius quam quisque. Enim ut sem viverra aliquet eget sit amet tellus. Ut diam quam nulla porttitor. Pharetra vel turpis nunc eget lorem dolor sed. Turpis massa sed elementum tempus egestas sed. Natoque penatibus et magnis dis parturient montes. Elit at imperdiet dui accumsan sit amet nulla facilisi. Vulputate dignissim suspendisse in est ante in nibh mauris. Eu facilisis sed odio morbi. Cum sociis natoque penatibus et magnis dis parturient. Sed vulputate odio ut enim blandit. Etiam dignissim diam quis enim lobortis scelerisque. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Id donec ultrices tincidunt arcu non sodales.</p></body></html>';
+  static const String termsPageContent =
+      '<html><body><h3>Terms of Use</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet. Nec dui nunc mattis enim ut tellus. Lacus suspendisse faucibus interdum posuere lorem ipsum. Augue neque gravida in fermentum et sollicitudin ac. Est ante in nibh mauris cursus mattis molestie a. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Semper eget duis at tellus at urna condimentum mattis pellentesque. Enim sed faucibus turpis in eu. At auctor urna nunc id cursus metus aliquam. <br><br>Sed pulvinar proin gravida hendrerit lectus. Varius quam quisque id diam vel quam elementum pulvinar etiam. Sed risus ultricies tristique nulla. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Cursus eget nunc scelerisque viverra mauris in aliquam. Purus sit amet volutpat consequat mauris nunc. Faucibus purus in massa tempor nec feugiat nisl pretium. Id interdum velit laoreet id. Auctor eu augue ut lectus. \n\n Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Eget lorem dolor sed viverra ipsum nunc aliquet. Leo duis ut diam quam. Scelerisque in dictum non consectetur a erat nam at lectus. Augue ut lectus arcu bibendum at varius vel. Arcu risus quis varius quam quisque. Enim ut sem viverra aliquet eget sit amet tellus. Ut diam quam nulla porttitor. Pharetra vel turpis nunc eget lorem dolor sed. Turpis massa sed elementum tempus egestas sed. Natoque penatibus et magnis dis parturient montes. Elit at imperdiet dui accumsan sit amet nulla facilisi. Vulputate dignissim suspendisse in est ante in nibh mauris. Eu facilisis sed odio morbi. Cum sociis natoque penatibus et magnis dis parturient. Sed vulputate odio ut enim blandit. Etiam dignissim diam quis enim lobortis scelerisque. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Id donec ultrices tincidunt arcu non sodales.</p></body></html>';
+
+  static const String onboardingTitle1 = 'Primeweb';
+  static const String onboardingTitle2 = 'Web to App';
+  static const String onboardingTitle3 = 'Webview App';
+
+  static const String onboardingDesc1 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.';
+  static const String onboardingDesc2 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ';
+  static const String onboardingDesc3 =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ';
+  static const String done = 'Done';
+}
